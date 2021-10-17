cf236f623cdd4546fa9e8a1ac7c8ce5e6b604525
Daniell <lealbeltran.daniel@gmail.com>
2021-10-17 16:05:31 -0500

    first commit

---
+++ src/main/java/Domingo_Reto3/Reto3/RepositorioCabin.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Domingo_Reto3.Reto3;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author USUARIO
+ */
+@Repository
+public class RepositorioCabin {
+    
+    @Autowired
+    private InterfaceCabin crud;
+
+    public List<Cabin> getAll(){
+        return (List<Cabin>) crud.findAll();
+    }
+
+    public Optional<Cabin> getcabin(int id){
+        return crud.findById(id);
+    }
+
+    public Cabin save(Cabin cabin){
+        return crud.save(cabin);
+    }
+    public void delete(Cabin cabin){
+        crud.delete(cabin);
+    }
+    
+}
+
